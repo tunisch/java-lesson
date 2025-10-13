3b5165ea803cb49fe87e4b8daace0a7e8186ba64
Tunahan Erturk <tunaerturk1@gmail.com>
2025-10-13 20:03:43 +0300

    new update and added code to BodyMassIndexCalculator

--- src/main/java/BodyMassIndexCalculator.java
+++ src/main/java/BodyMassIndexCalculator.java
@@ -2,19 +2,30 @@ import java.util.Scanner;
 
 public class BodyMassIndexCalculator {
     public static void main(String[] args) {
-     double boy, kilo, bmi;
+        double boy, kilo, bmi, idealKilo, idealBmi = 23;
 
-     Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-     System.out.print("Lutfen Boyunuzu (metre cinsinden) giriniz : ");
-     boy = input.nextDouble();
+        System.out.print("Lutfen Boyunuzu (metre cinsinden) giriniz : ");
+        boy = input.nextDouble();
 
-     System.out.print("Lutfen Kilonuzu giriniz : ");
-     kilo = input.nextDouble();
+        System.out.print("Lutfen Kilonuzu giriniz : ");
+        kilo = input.nextDouble();
 
-     bmi = kilo / (boy * boy);
-     System.out.println("Vucut Kitle indeksiniz : " + bmi);
+        bmi = kilo / (boy * boy);
+        System.out.println("Vucut Kitle indeksiniz : " + bmi);
 
+        String hesap = (bmi <= 18.5) ? "Zayifsin" :
+                (18.5 <= bmi && bmi <= 24.9) ? "Normal" :
+                (bmi < 29.9) ? "fazla kilolu" :
+                (bmi<= 34.9) ? "1.derece obezite " :
+                (bmi <= 39.9) ? "2.derece obezite" :
+                (40.0 <= bmi) ? "3.derece obezite" : "Allah Sifa versin";
+        System.out.println("Saglik Durumunuz : " + hesap);
+
+
+        idealKilo = idealBmi * (boy * boy);
+        System.out.println("Ideal kilonuz : " + idealKilo);
 
     }
 }

b8d6da8c497812a28a5b8ef426ae70c84decedfa
Tunahan Erturk <tunaerturk1@gmail.com>
2025-10-18 11:16:16 +0300

    SwitchCase changed break design

--- src/main/java/SwitchCase.java
+++ src/main/java/SwitchCase.java
@@ -4,10 +4,13 @@ public class SwitchCase {
         switch(points)
         {
             case 6: ; //komut ve break yok bu yuzden derleyici burayi direkt gecer.
-            case 7: System.out.println("Java");break; // break var komut var o yuzden ilkde burasi oldugu icin burayi derler.(yanlis olsa bile)
+            case 7: System.out.println("Java");
+            break; // break var komut var o yuzden ilkde burasi oldugu icin burayi derler.(yanlis olsa bile)
             case 8: ;
-            case 9: System.out.println("101");break;
-            case 10: System.out.println("Patika"); break;
+            case 9: System.out.println("101");
+            break;
+            case 10: System.out.println("Patika");
+            break;
             default: System.out.println("Dev");
         }
 

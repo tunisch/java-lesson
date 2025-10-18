7fafd0a8a6ced09575ac76b9cd92aec36da09c93
Tunahan Erturk <tunaerturk1@gmail.com>
2025-10-18 11:11:11 +0300

    if-else examples meaning and examples.

--- src/main/java/IfElse.java
+++ src/main/java/IfElse.java
@@ -1,13 +1,12 @@
 public class IfElse {
-    public static void main (String[] args){
+    public static void main(String[] args) {
 
         int a = 5, b = 4;
 
-        if (a == b){
+        if (a == b) {
             System.out.println("a ve b esit");
-        }else{
+        } else {
             System.out.println("a ve b esit degil");
         }
-
     }
-
+}

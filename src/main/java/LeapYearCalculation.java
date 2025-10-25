858378686bb155ddea3da187eec757d36dfc1b5a
Tunahan Erturk <tunaerturk1@gmail.com>
2025-10-25 16:06:54 +0300

    Leap Year Calculate with if-else

--- src/main/java/LeapYearCalculation.java
+++ src/main/java/LeapYearCalculation.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class LeapYearCalculation {
     public static void main(String[] args) {
+        int year;
 
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Yıl giriniz: ");
+        year = inp.nextInt();
+
+        if (year % 400 == 0) {
+            System.out.println(year + " bir artık yıldır!");
+        } else if (year % 100 == 0) {
+            System.out.println(year + " bir artık yıl değildir!");
+        } else if (year % 4 == 0) {
+            System.out.println(year + " bir artık yıldır!");
+        } else {
+            System.out.println(year + " bir artık yıl değildir!");
+        }
+        inp.close();
     }
 }
+
+

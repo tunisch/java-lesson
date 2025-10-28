40b3be36f32bd911c5d327cda1cf60faff1a75a2
Tunahan Erturk <tunaerturk1@gmail.com>
2025-10-28 11:18:12 +0300

    while example : password

---
+++ src/main/java/WhileLoop.java
@@ -0,0 +1,86 @@
+/* Basic WhileLopp Examp */
+//public class WhileLoop {
+//    public static void main(String[] args) {
+//
+//        int i = 1;
+//
+//        System.out.println("Program basladi.");
+//        while(i <= 5){ // while ici her zaman true olmaliki dongu calisabilsin!!
+//            System.out.println(i);
+//            i++;
+//        }
+//        System.out.println("Program bitti.");
+//    }
+//}
+
+/* Nested While Loop Example */
+
+//public class WhileLoop {
+//    public static void main(String[] args) {
+//        int i = 1;
+/// /      int k = 1;
+//        System.out.println("Program basladi.");
+//        while(i <= 5){ // while ici her zaman true olmaliki dongu calisabilsin!!
+//            System.out.println(i);
+//            int k = 1; //ic ice dongu olmasi icin ic dongu degeri burda tanimlariz.
+//            while(k <= 10){
+//                System.out.print(k + ",");
+//                k+=2; //k = k + 2 demektir k yi ikiser arttir
+//            }
+//            System.out.println();
+//            i++; // i = i + 1 demektir iyi birer arttir demektir.
+//        }
+//        System.out.println("Program bitti.");
+//    }
+//}
+
+/* example to Password with While : The user asks the programmer until he enters the correct password */
+
+import java.util.Scanner;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+
+        int password;
+        boolean isPasswordSuccess = false; // paraola durumu false ise kullanicidan sifre girmesini saglaycagiz o yuzden false girdik.
+
+        Scanner inp = new Scanner(System.in);
+
+        while (!isPasswordSuccess) { // while ici (condition) her zaman true olmaliki dongu calisabilsin!!
+            System.out.print("Sifrenizi giriniz: ");
+            password = inp.nextInt();
+            if (password == 123) {
+                System.out.println("Dogru");
+                isPasswordSuccess = true;
+            } else {
+                System.out.println("Yanlis");
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

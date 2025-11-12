cac6c6b473915b916388ea441fc2adef11d9c0ca
Tunahan Erturk <tunaerturk1@gmail.com>
2025-11-12 13:51:28 +0300

    Return and Void using

---
+++ src/main/java/ReturnAndVoid.java
@@ -0,0 +1,31 @@
+/* Result metodu */
+
+//public class ReturnAndVoid {
+//    static int sum(int a, int b) {
+//        int result = a + b;
+//        System.out.println(result);
+//        return a + b; // ✅ Sonucu geri gönder;
+//    }
+//
+//    public static void main(String[] args) {
+//        int result = sum(5, 2) + sum(3, 1);
+//        System.out.println(result);
+//    }
+//}
+
+/* Void metodu */
+
+public class ReturnAndVoid {
+
+    static void show() {
+        System.out.println("Show metodu..");
+        return; //return; → ✅ Geçerli (sadece metodu erken bitirir)
+        /* Void metotlar değer döndüremez, ama return; ifadesiyle erken çıkış yapılabilir.*/
+
+    }
+
+    public static void main(String[] args) {
+        show();
+    }
+}
+

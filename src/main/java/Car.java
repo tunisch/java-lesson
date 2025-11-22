4253979989893df550e472f70d535cf8a820572b
Tunahan Erturk <tunaerturk1@gmail.com>
2025-11-22 14:00:21 +0300

    What is the Class? Class identified

---
+++ src/main/java/Car.java
@@ -0,0 +1,25 @@
+class Car {
+    //Nitelikler
+    String type;
+    String model;
+    String color;
+    int speed;
+    int speedLimit = 180;
+
+    //Davranislar
+    void increaseSpeed(int increment) {
+        if ((speed + increment) < speedLimit) {
+            speed += increment;
+        }
+    }
+
+    void decreaseSpeed(int decrease) {
+        if (speed > 0) {
+            speed -= decrease;
+        }
+    }
+
+    void printSpeed() {
+        System.out.println("Hiziniz: " + speed);
+    }
+}

d48cbc681cc74fe214536f999786f4b05ead4a47
Tunahan Erturk <tunaerturk1@gmail.com>
2025-12-08 15:22:55 +0300

    Arrays Class and Special Methods and Examples

--- src/main/java/HelperArray.java
+++ src/main/java/HelperArray.java
@@ -5,13 +5,13 @@ public class HelperArray {
           " HelperArray helper = new HelperArray(); " buna gerek kalmadan -> HelperArray.print(list); diyerek artik boyle cagirabiliriz !!!
      */
 
-//    void print(int[] arr) {
-//        System.out.print("[");
-//        for (int i = 0; i < arr.length; i++) {
-//            System.out.print(arr[i] + " ");
-//        }
-//        System.out.print("]");
-//    }
+    static void print(int[] arr) {
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.print("]");
+    }
 
 //    static void print(double[] arr) { // static olarak kullandik double ile olan metodu helper nesnessine gerek kalmadan class adi ile printleyebiliriz!!
 //        System.out.print("[");
@@ -20,4 +20,57 @@ public class HelperArray {
 //        }
 //        System.out.print("]");
 //    }
+
+//    static void fill(int[] arr, int value) {
+//        System.out.print("[");
+//        for (int i = 0; i < arr.length; i++) {
+//            arr[i] = value;
+//            System.out.print(arr[i] + " ");
+//        }
+//        System.out.print("]");
+//    }
+
+    static int[] fill(int[] arr, int value) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = value;
+        }
+        return arr;
+    }
+
+    static int search(int[] arr, int value) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == value) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    static int[] copy(int[] arr, int limit) {
+        int[] arrY = new int[limit];
+        for (int i = 0; i < limit && i < arr.length; i++) { // i < limit → yeni dizinin dışına taşmanı engelliyor , i < arr.length → eski dizinin dışına taşmanı engelliyor
+            arrY[i] = arr[i];
+        }
+        return arrY;
+    }
+
+    static int[] copyOfRangeBenim(int[] arr, int start, int end) {
+        int[] arrT = new int[end - start];
+        for (int i = start, j = 0; i < end; i++, j++) {
+            arrT[j] = arr[i];
+        }
+        return arrT;
+    }
+
+    static boolean equals(int[] l1, int[] l2) {
+        if (l1.length != l2.length) {
+            return false;
+        }
+        for (int i = 0; i < l1.length; i++) {
+            if (l1[i] != l2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -10,30 +10,99 @@ public class MainClass {
     public static void main(String[] args) {
 
         int[] list = {1, 2, 3, 4};
-        int[] list2 = {4, 5, 6, 8};
+        int[] list2 = {4, 5, 6, 8, 9, 10, 13, 15, 17};
+        int[] liste = {15, 1, 99, 7, 7, -22, 11, 2, -49, 52};
+        int[] list4 = {6, 1, 55, 21, 33, -321, -21, 2, -11, 27};
+        int[] list5 = {2, 3, 4, 5, 6, 7, 8};
+        int[] list6 = {6, 7, 8, 9, 10, 11};
         double[] list3 = {4.3, 5.2, 6.1};
+        int[] list7 = {1, 2, 3};
+        int[] list8 = {1, 2, 3};
+        int[] list9 = {1, 2, 10,5};
 //
 //        HelperArray helper = new HelperArray();
 //        helper.print(list); //print metodunun basina "static" koayrsak helper nesnesi olusturmamiza gerek kalmaz
 //        System.out.println();
 //        helper.print(list2);
 //        System.out.println();
-       // HelperArray.print(list3); // static metod olarak yazdigimiz icin "helper" nesnesine gerek kalmadi!!!
-       // System.out.println();
-/* Dizinin elemanlarini ekrana direkt bastiran method
-- Yeni bir nesne uretmeye method uretmeye gerek kalmadan ekran bastiririz!!
-    Arrays.toString()
-*/
+        // HelperArray.print(list3); // static metod olarak yazdigimiz icin "helper" nesnesine gerek kalmadi!!!
+        // System.out.println();
+        /* Dizinin elemanlarini ekrana direkt bastiran method
+        - Yeni bir nesne uretmeye method uretmeye gerek kalmadan ekran bastiririz!!
+        Arrays.toString()
+        */
 
         System.out.println(Arrays.toString(list));
         // override a gerek kalmadan Arrays.toString yazarak double da cagirilip printlenebilir
 
         System.out.println(Arrays.toString(list3));
 
+        /* Arrays.fill metodu ile dizilerimizin belirli bir bölümlerine değerler atayabiliriz.
+        Arrays.fill()
+        */
+        System.out.println(Arrays.toString(liste)); //once varsayilan degerleri verelim
+        Arrays.fill(liste, 2); // tum elemanlari 2 yaptik
+        System.out.println(Arrays.toString(liste));
+
+//        HelperArray.fill(list2,4);
+
+        HelperArray.print((HelperArray.fill(list2, 4)));
+
+        /* Arrays.fill() kulanirken aralik vererek te degerleri degistirebiliriz!!  */
+
+        Arrays.fill(list2, 2, 5, 11);
+        System.out.println(Arrays.toString(list2));
+
+        // DIZILERDE EKRANA PRINT YAP<AYA YARAR!!!
+        // System.out.println(Arrays.toString());
+        /* Arrays.sort() Siralama methodu
+        - Arrays.sort() metodu ile dizilerdeki elemanları sıralayabiliriz.
+         */
+        System.out.println(Arrays.toString(list4)); //siralanmadan onceki hali
+        Arrays.sort(list4);
+        System.out.println(Arrays.toString(list4));
+
+        /* Arrays.binarySearch()
+        - Java'da dizideki bir elemanın indis değerini bulmak için binarySearch kullanılabilir.
+        - Ama bu metodu kullanabilmek için, dizinin sıralı olması gerekmektedir.
+         */
+        System.out.println(Arrays.binarySearch(list4, 55));
+
+        //Ayni durum Arrays methodsuz kullanimi
+        System.out.println(HelperArray.search(list4, 21));
+
+        /* Arrays.copyOf() ve Arrays.copyOfRange() metotu
+        - Mevcut diziden belli bir uzunlukta yeni bir dizi oluşturmak için Arrays.copyOf() metotu kullanılır
+        - Mevcut diziden belli bir aralıkta yeni bir dizi oluşturmak için ise Array.copyOfRange() metodu kullanılır.
+         */
 
 
+        //araliga kadar kopyalamak istersek soldan indis sayisinca verilir
+        int[] copyList4 = Arrays.copyOf(list4, 4);
+        System.out.println(Arrays.toString(copyList4));
 
+        //ayni boyutta kopyalamak istersek list.length deriz
+        int[] copyList = Arrays.copyOf(list4, list4.length);
+        System.out.println(Arrays.toString(copyList));
 
+        // aralikli kopyalamak istersek copyofRange() kukllanilir , aralik indisleri verilirek yapilir !!
+        int[] copyliste = Arrays.copyOfRange(list4, 2, 6);
+        System.out.println(Arrays.toString(copyliste));
+
+        //Ayni durum copyof icin Arrays methodsuz kullanimi
+        System.out.println(Arrays.toString(HelperArray.copy(list5, 4)));
+
+        //Ayni durum copyofRange icin Arrays methodsuz kullanimi
+        System.out.println(Arrays.toString(HelperArray.copyOfRangeBenim(list6, 1, 4)));
+
+        /*Arrays.equals() Methodu
+         - Java'da iki dizinin eşitliğini kontrol etmek için Arrays.equals() metotu kullanılır.
+         */
+
+        System.out.println(Arrays.equals(list7,list8));
+
+        // Ayni durum Arrays.equals olmadan methodsuz kulanimi kendi methodumuzla kullanimi yani
+        System.out.println(HelperArray.equals(list7,list8));
 
     }
 }

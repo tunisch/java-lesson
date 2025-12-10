919f40f7703309f950423538e8b5ac056cf616f5
Tunahan Erturk <tunaerturk1@gmail.com>
2025-12-10 10:54:50 +0300

    ArraysClassAndMethods Examples = 3

---
+++ src/main/java/ArraysClassAndMethodExamp3.java
@@ -0,0 +1,24 @@
+/*   Ödev - Dizideki Elemanları Sıralama      */
+
+/* Java dilinde, dizideki elemanları küçükten büyüğe sıralayan programı yazınız. Dizinin boyutunu ve dizinin elemanlarını kullanıcıdan alınız. */
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArraysClassAndMethodExamp3 {
+    public static void main(String[] args) {
+
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Dizinin boyutunu giriniz: ");
+
+        int x = inp.nextInt();
+        int[] arr = new int[x];
+
+        for (int i = 1; i < x + 1; i++) {
+            System.out.print(i + ".nci elemani giriniz: ");
+            arr[i - 1] = inp.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}

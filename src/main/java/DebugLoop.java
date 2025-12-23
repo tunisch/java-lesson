ef53521218ffc14a1d9be4159a49e03f99427377
Tunahan Erturk <tunaerturk1@gmail.com>
2025-12-23 10:30:55 +0300

    while loop crash problem added

--- src/main/java/DebugLoop.java
+++ src/main/java/DebugLoop.java
@@ -4,5 +4,10 @@ public class DebugLoop {
         for (int i = 0; i < 5; i++) {
             System.out.println("i = " + i);
         }
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i);
+        }
+
     }
 }

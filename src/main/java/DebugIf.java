846d8258a822b86100db75a4ea68c0797a32d3ff
Tunahan Erturk <tunaerturk1@gmail.com>
2025-12-23 10:09:26 +0300

    debug training other examples

---
+++ src/main/java/DebugIf.java
@@ -0,0 +1,12 @@
+public class DebugIf {
+    public static void main(String[] args) {
+
+        int age = 17;
+
+        if (age > 18) {
+            System.out.println("Girebilirsin");
+        } else {
+            System.out.println("Giremezsin");
+        }
+    }
+}

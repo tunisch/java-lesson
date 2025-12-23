7c228a9e27ac57c0dad16f8b568c1525edee582a
Tunahan Erturk <tunaerturk1@gmail.com>
2025-12-23 10:10:28 +0300

    DebugLogic examples debug training

---
+++ src/main/java/DebugLogic.java
@@ -0,0 +1,14 @@
+public class DebugLogic {
+    public static void main(String[] args) {
+
+        int score = 60;
+
+        if (score > 50) {
+            System.out.println("Gecti");
+        }
+
+        if (score > 70) {
+            System.out.println("Basarili");
+        }
+    }
+}

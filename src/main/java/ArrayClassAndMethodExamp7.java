e7867965ba0542f19fec573c1f0214241630a517
Tunahan Erturk <tunaerturk1@gmail.com>
2025-12-26 15:56:26 +0300

    arrange continuening

--- src/main/java/ArrayClassAndMethodExamp7.java
+++ src/main/java/ArrayClassAndMethodExamp7.java
@@ -1,40 +1,48 @@
 /* Pratik - Dizideki Tekrar Eden Sayıları Bulan Program */
 
 public class ArrayClassAndMethodExamp7 {
-
-    static boolean isFind(int[] arr, int value) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == value) { // dizi icinde value degeri var mi yok mu kontrolu
+    static boolean isDublicate(int[] arr, int value) {
+        // for each ile
+        // tip eleman : dizi
+        for (int a : arr) {
+            if (a == value) {
                 return true;
             }
         }
+        // for each olmadan
+//        for (int a = 0; a < arr.length; a++) {
+//            if (arr[a] == value) {
+//                return true;
+//            }
         return false;
     }
 
-
     public static void main(String[] args) {
-        int[] arr = {1, 2, 4, 6, 32, 3, 34, 33, 32, 10, 10, 11, 23, 32, 11};
+        int[] arr = {4, 6, 4, 6, 4, 6};
         int[] dublicate = new int[arr.length];
 
         for (int i = 0; i < arr.length; i++) {
+            // for döngüsünde arttırma (j++),
+            //sadece döngü gövdesi çalıştıktan sonra olur yani j< ------ durumu true ise j ++ calisir !!!
+            //
+            //Koşul en başta false ise:
+            //
+            //gövdeye girilmez
+            //
+            //arttırma da olmaz
+            for (int j = 0; j < i; j++) {
+                // cok tekrar eder tercih edilmez tekrar edenleri bir kere yazdircaz bu yuzden
+                // + yardimci diziye ve methoda ihtiyacimizi var
+///                if(arr[i] == arr[j]){
+                    System.out.println("Tekrar edenler" + arr[i]);
+///                }
 
-            for (int j = i + 1; j < arr.length; j++) { //
-
-                if (arr[i] == arr[j]) {
-
-                    // Daha önce duplicate'e eklenmiş mi?
-                    if (!isFind(dublicate, arr[i])) {
-                        dublicate[i] = arr[i];
+                if (isDublicate(dublicate, arr[i])) {
+                    if (arr[i] == arr[j]) {
+                        System.out.println("Tekrar edenler : " + arr[i]);
                     }
                 }
             }
         }
-
-        for (int i = 0; i < dublicate.length; i++) {
-            if (dublicate[i] != 0) {
-                System.out.print(dublicate[i] + " ");
-            }
-        }
     }
-
 }

9d1b8f52a165177ceb3009fe0b54f54b41a5e504
Tunahan Erturk <tunaerturk1@gmail.com>
2026-01-29 23:20:48 +0300

    the version before the command line was added

--- src/main/java/ArraysClassAndMethodExamp9.java
+++ src/main/java/ArraysClassAndMethodExamp9.java
@@ -5,42 +5,15 @@ import java.util.Arrays;
 
 public class ArraysClassAndMethodExamp9 {
 
-    static boolean isDuplicate(int[] arr, int index, int value) {
-        for (int i = 0; i < index; i++) {
-            if (arr[i] == value) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     public static void main(String[] args) {
 
         int[] arr = {10, 20, 20, 10, 10, 20, 5, 20};
-
-        int againIndex = 0;
-        int[] againArr = new int[arr.length];
-        boolean isAgain = false;
-        int count = 0;
+        int[] freq = new int[arr.length];
 
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < i; j++) {
-                isAgain = false;
-                if (arr[i] == arr[j]) {
-                    if (!isDuplicate(againArr, againIndex, arr[j])) {
-                        againArr[againIndex] = arr[j];
-                        againIndex++;
-                        isAgain = true;
-                        count++;
-                    }
-                }
+            if(freq[i] != 0){ // daha once gordum mu
+
             }
-            if (isAgain) {
-                System.out.println(arr[i] + " sayisindan " + count + " tekrar oldu ");
-            }
-        }
-        for (int i = 0; i < againIndex; i++) {
-            System.out.print(againArr[i] + " ");
         }
     }
 }

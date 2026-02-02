7ffd1ce236fb2fce429ba46cc01da5f4bb5a8f10
Tunahan Erturk <tunaerturk1@gmail.com>
2026-02-02 22:44:28 +0300

    Calculating the repetition of elements in an array. but then the duplicate status will be added as a method.

--- src/main/java/ArraysClassAndMethodExamp9.java
+++ src/main/java/ArraysClassAndMethodExamp9.java
@@ -5,15 +5,22 @@ import java.util.Arrays;
 
 public class ArraysClassAndMethodExamp9 {
 
+
     public static void main(String[] args) {
 
         int[] arr = {10, 20, 20, 10, 10, 20, 5, 20};
-        int[] freq = new int[arr.length];
+        int count = 0;
 
         for (int i = 0; i < arr.length; i++) {
-            if(freq[i] != 0){ // daha once gordum mu
-
+            count = 0;
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                }
             }
+
+            System.out.println(arr[i] + " " + count + " tekrar etti");
         }
     }
 }
+

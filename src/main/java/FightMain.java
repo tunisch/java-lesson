2aad70973a89bc95cb2cc82ac8c286fc017cc8ca
Tunahan Erturk <tunaerturk1@gmail.com>
2026-02-08 12:52:34 +0300

    Fight Main created for Boxing Game , Fight Main manages Boxing Game

---
+++ src/main/java/FightMain.java
@@ -0,0 +1,11 @@
+public class FightMain {
+    public static void main(String[] args) {
+        Fighter f1 = new Fighter("Alex", 5, 100, 80, 0.7);
+        Fighter f2 = new Fighter("Jack", 10, 90, 90, 0.3);
+        f1.hit(f2);
+
+        Ring r1 = new Ring(f1, f2, 75,95);
+        r1.startFight(); // fight baslatmaya yarar
+
+    }
+}
